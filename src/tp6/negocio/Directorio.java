a887c3170652e7a534937d944b1dc7878f44f9da
MiguelLuceroSL <theflixgame8@gmail.com>
2023-04-21 16:43:47 -0300

    Update grupo borrarCliente()
    Aca le dejamos el metodo  borrarCliente()

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -6,4 +6,16 @@ import java.util.HashMap;
 
 public class Directorio {
     private HashMap<String,Cliente> registro=new HashMap<String,Cliente>();
+    
+    
+    
+    //Metodos
+    public void borrarCliente(String telefono){
+        if (registro.containsKey(telefono)) {
+            registro.remove(telefono);
+            System.out.println("Se ha eliminado correctamente.");
+        } else {
+            System.out.println("Cliente no encontrado.");
+        }
+    }
 }
