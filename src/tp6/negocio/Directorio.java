d232e81f6eb935222d8cb352867852cd97bbf854
Luis Javier Mercado <50953075+xluis1973@users.noreply.github.com>
2023-04-21 17:17:50 -0300

    todo

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -11,6 +11,32 @@ import java.util.Map;
 public class Directorio {
     private HashMap<String,Cliente> registro=new HashMap<String,Cliente>();
     
+    public Cliente buscarCliente(String numTelefono){
+        for (Map.Entry<String, Cliente> entry : registro.entrySet()) {
+            String key = entry.getKey();
+            Cliente value = entry.getValue();
+            if(numTelefono.equals(key)){
+                return value;
+            }
+        }
+        System.out.println("No se encontrò el telefono asociado a un cliente");
+        return null;
+    }
+
+
+
+
+//AgregarCliente() que permite registrar un nuevo cliente con su respectivo nro de 
+//teléfono. Siendo el nro del teléfono la clave del mismo.
+    public boolean agregarCliente(String telefono, Cliente c) {
+        boolean agregado = false;
+        if (!registro.containsKey(telefono)) {
+            registro.put(telefono, c);
+            agregado = true;
+        }
+        return agregado;
+    }   
+    
     public ArrayList<String> buscarTelefono(String apellido){
         ArrayList <String> num= new ArrayList<String>();
         for (Map.Entry<String, Cliente> entry : registro.entrySet()) {
