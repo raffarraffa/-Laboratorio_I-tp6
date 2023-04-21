bb12b5ff14e2c109d2115fb3a98247c4512ab41c
Luis Javier Mercado <50953075+xluis1973@users.noreply.github.com>
2023-04-21 16:27:54 -0300

    update Directorio

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tp6.negocio;
 
-/**
- *
- * @author Usuario
- */
+import java.util.HashMap;
+
+
 public class Directorio {
-    
+    private HashMap<String,Cliente> registro=new HashMap<String,Cliente>();
 }
