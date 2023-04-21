56c2cfb5474fdc4fda24f27c1c86e22df4be592d
Santiago <santigonzalez555@gmail.com>
2023-04-21 16:43:42 -0300

    GRUPO1
    agregarCliente()

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -6,4 +6,17 @@ import java.util.HashMap;
 
 public class Directorio {
     private HashMap<String,Cliente> registro=new HashMap<String,Cliente>();
+    
+    
+//AgregarCliente() que permite registrar un nuevo cliente con su respectivo nro de 
+//teléfono. Siendo el nro del teléfono la clave del mismo.
+    
+    public boolean agregarCliente (String telefono, Cliente c){
+        boolean agregado = false;
+        if (!registro.containsKey(telefono)) {
+            registro.put(telefono, c); 
+            agregado = true;
+        }
+        return agregado;
+    }
 }
