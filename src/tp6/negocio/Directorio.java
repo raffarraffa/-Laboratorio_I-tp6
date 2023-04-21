152b46e8f235fcd5743a0fe8f696aa43d7546821
PaulaPriotti <paaupriotti@gmail.com>
2023-04-21 16:54:06 -0300

    buscarCliente()

--- src/tp6/negocio/Directorio.java
+++ src/tp6/negocio/Directorio.java
@@ -2,8 +2,22 @@
 package tp6.negocio;
 
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class Directorio {
     private HashMap<String,Cliente> registro=new HashMap<String,Cliente>();
+    
+    
+    public Cliente buscarCliente(String numTelefono){
+        for (Map.Entry<String, Cliente> entry : registro.entrySet()) {
+            String key = entry.getKey();
+            Cliente value = entry.getValue();
+            if(numTelefono.equals(key)){
+                return value;
+            }
+        }
+        System.out.println("No se encontrò el telefono asociado a un cliente");
+        return null;
+    }
 }
