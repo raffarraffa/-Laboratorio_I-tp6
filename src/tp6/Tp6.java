511a0b3815abefd15c4b335e947c8fc25744fb14
Luis Javier Mercado <50953075+xluis1973@users.noreply.github.com>
2023-04-21 18:13:35 -0300

    update

--- src/tp6/Tp6.java
+++ src/tp6/Tp6.java
@@ -5,6 +5,10 @@
  */
 package tp6;
 
+import javax.swing.JOptionPane;
+import tp6.negocio.Cliente;
+import tp6.negocio.Directorio;
+
 /**
  *
  * @author Usuario
@@ -16,6 +20,28 @@ public class Tp6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Directorio d=new Directorio();
+        //Cliente martin lucero de la punta mod 11 mz 33 dni 123 telefono 4242
+        boolean resultado=d.agregarCliente("4242",new Cliente(123,"martin","lucero","la punta","mod 11 mz 33") );
+       if(resultado){
+       
+           JOptionPane.showMessageDialog(null,"Dato registrado");
+       }else{
+       
+           JOptionPane.showMessageDialog(null,"Dato No registrado");
+           
+       }
+       boolean resultado2=d.agregarCliente("4242",new Cliente(333,"pepe","lucero","la punta","mod 11 mz 33") );
+       if(resultado2){
+       
+           JOptionPane.showMessageDialog(null,"Dato registrado");
+       }else{
+       
+           JOptionPane.showMessageDialog(null,"Dato No registrado");
+           
+       }
+        
+        
     }
     
 }
